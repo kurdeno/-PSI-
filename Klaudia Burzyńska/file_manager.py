e0e6a0aad9615a9327f42653db22cd3b350ed4d4
Klaudia Burza <klaudiab.97@wp.pl>
2020-11-09 18:19:01 +0100

    Wprowadzenie do Pythona 2

--- Klaudia Burzyńska/file_manager.py
+++ Klaudia Burzyńska/file_manager.py
@@ -3,13 +3,12 @@ class FileManager:
         self.file_name = file_name
 
     def read_file(self):
-        file1=open(self.file_name)
+        file1 = open(self.file_name)
         file_text = file1.read()
         print(file_text)
         file1.close()
 
     def update_file(self, text_data):
-        file1=open(self.file_name, "a")
+        file1 = open(self.file_name, "a")
         file1.write(text_data)
         file1.close()
-
